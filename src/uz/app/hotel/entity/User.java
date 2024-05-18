e835fcaa3803279ea7e4e4df12f0046cc5589c93
Aziza <azizaxonrahimberdiyeva@gmail.com>
2024-05-18 15:19:40 +0500

    first

---
+++ src/uz/app/hotel/entity/User.java
@@ -0,0 +1,52 @@
+package uz.app.hotel.entity;
+
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+
+public class User {
+    private final String  id= randomUUID().toString();
+    private String name;
+    private String username;
+    private String password;
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
